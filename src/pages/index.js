8213cd43d808be6d7ae5fc2d7586350ae0cd9e06
Hermetheus <allanmolson@gmail.com>
2019-08-24 14:48:34 -0500

    Create Background Section w/  styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,30 @@
+import { graphql } from "gatsby"
 import React from "react"
-import { FaGulp } from "react-icons/fa"
+import BackgroundSection from "../components/global/BackgroundSection"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <FaGulp />
+    <BackgroundSection
+      img={data.img.childImageSharp.fluid}
+      title="regular joe's"
+      styleClass="default-background"
+    />
   </Layout>
 )
+
+export const query = graphql`
+  {
+    img: file(relativePath: { eq: "default-background.jpeg" }) {
+      childImageSharp {
+        fluid {
+          ...GatsbyImageSharpFluid_tracedSVG
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage

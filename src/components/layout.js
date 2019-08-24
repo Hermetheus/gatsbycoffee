92019162c25e8cf7f2ac9a8b02b6db35255fa3eb
Hermetheus <allanmolson@gmail.com>
2019-08-24 14:00:16 -0500

    Navbar Made

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,10 +7,16 @@
 
 import PropTypes from "prop-types"
 import React from "react"
+import Navbar from "../components/global/Navbar"
 import "./bootstrap.min.css"
 import "./layout.css"
 
-const Layout = ({ children }) => <>{children}</>
+const Layout = ({ children }) => (
+  <>
+    <Navbar />
+    {children}
+  </>
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
